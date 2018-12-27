0f435905e191bc6669ac71895ecad8afc2a05bf9
Magne Nordaas <magne.nordaas@gmail.com>
2018-12-27 16:47:10 +0100

    Rewrite extract_geokeys

--- src/rasputin/reader.py
+++ src/rasputin/reader.py
@@ -1,6 +1,7 @@
 from typing import Tuple, Dict, Any, Optional
 from enum import Enum
 from PIL import Image
+from PIL.TiffImagePlugin import TiffImageFile
 import json
 from itertools import islice
 import numpy as np
@@ -9,33 +10,114 @@ from logging import getLogger
 from rasputin import triangulate_dem
 
 
-class AsciiGeoKeys(Enum):
-    GTCitationGeoKey = 1026
-    GeogCitationGeoKey = 2049
+class KeyValueTags(Enum):
+    # Parameter value types:
+    #    'short'  values are stored in the geokey directory
+    #    'double'                   in tag 34736 at given offset
+    #    'ascii'                    in tag 34737 at offset:offset+count
+    GeoShortParamsTag = 0
+    GeoDoubleParamsTag = 34736
+    GeoAsciiParamsTag = 34737
 
 
-class CodedGeoKeys(Enum):
+class GeoKeys(Enum):
+    # Configuration keys
     GTModelTypeGeoKey = 1024
     GTRasterTypeGeoKey = 1025
+    GTCitationGeoKey = 1026
+
+    # Geographic CS parameter keys
+    GeographicTypeGeoKey = 2048
+    GeogCitationGeoKey = 2049
+    GeogGeodeticDatumGeoKey = 2050
+    GeogPrimeMeridianGeoKey = 2051
+    GeogPrimeMeridianLongGeoKey = 2061
+    GeogLinearUnitSizeGeoKey = 2053
     GeogAngularUnitsGeoKey = 2054
+    GeogAngularUnitSizeGeoKey = 2055
+    GeogEllipsoidGeoKey = 2056
+    GeogSemiMajorAxisGeoKey = 2057
+    GeogSemiMinorAxisGeoKey = 2058
+    GeogInvFlatteningGeoKey = 2059
+    GeogAzimuthUnitsGeoKey = 2060
+
+    # Projected CS parameters keys
     ProjectedCSTypeGeoKey = 3072
+    PCSCitationGeoKey = 3073
+
+    # Projection definition keys
+    ProjectionGeoKey = 3074
+    ProjCoordTransGeoKey = 3075
     ProjLinearUnitsGeoKey = 3076
+    ProjLinearUnitSizeGeoKey = 3077
+    ProjStdParallel1GeoKey = 3078
+    ProjStdParallel2GeoKey = 3079
+    ProjNatOriginLongGeoKey = 3080
+    ProjNatOriginLatGeoKey = 3081
+    ProjFalseEastingGeoKey = 3082
+    ProjFalseNorthingGeoKey = 3083
+    ProjFalseOriginLongGeoKey = 3084
+    ProjFalseOriginLatGeoKey = 3085
+    ProjFalseOriginEastingGeoKey = 3086
+    ProjFalseOriginNorthingGeoKey = 3087
+    ProjCenterLongGeoKey = 3088
+    ProjCenterLatGeoKey = 3089
+    ProjCenterEastingGeoKey = 3090
+    ProjCenterNorthingGeoKey = 3091 # Incorrectly named in document
+    ProjScaleAtNatOriginGeoKey = 3092
+    ProjScaleAtCenterGeoKey = 3093
+    ProjAzimuthAngleGeoKey = 3094
+    ProjStraightVertPoleLongGeoKey = 3095
 
 
-def extract_geo_keys(*, named_tags: dict) -> Dict[str, Any]:
-    geo_key_directory = np.array(named_tags["GeoKeyDirectoryTag"]).reshape(-1, 4)
-    geo_keys = geo_key_directory[:, 0]
-    info = {}
-    for k in AsciiGeoKeys:
-        if k.value in geo_keys:
-            row = geo_key_directory[np.where(k.value == geo_keys)].flatten()
-            if row[1] == 34737:
-                info[k.name] = named_tags["GeoAsciiParamsTag"][0][row[3]: row[3] + row[2]]
-    for k in CodedGeoKeys:
-        if k.value in geo_keys:
-            row = geo_key_directory[np.where(k.value == geo_keys)].flatten()
-            info[k.name] = row[3]
-    return info
+def extract_geo_keys(*, image: TiffImageFile) -> Dict[str, Any]:
+    """ Extract GeoKeys from image and return as Python dictionary. """
+
+    # Using .tag_v2 instead of legacy .tag
+    image_tags = image.tag_v2
+    try:
+        GeoKeyDirectory = np.asarray(image_tags[34735], dtype="ushort").reshape((-1,4))
+    except KeyError:
+        raise RuntimeError("Image is missing GeoKeyDirectory required by GeoTiff v1.0.")
+
+    Header = GeoKeyDirectory[0,:]
+    KeyDirectoryVersion = Header[0]
+    KeyRevision = (Header[1], Header[2])
+    NumberOfKeys = Header[3]
+
+    assert KeyDirectoryVersion == 1 # Only existing version
+    assert len(GeoKeyDirectory) == NumberOfKeys + 1
+
+    # Read all the geokey fields
+    geo_keys = {}
+    for (key_id, location, count, value_offset) in GeoKeyDirectory[1:]:
+        key_name = GeoKeys(key_id).name
+
+        if KeyValueTags(location) == KeyValueTags.GeoShortParamsTag:
+            # Short values are stored in value_offset field in the directory
+            key_value = value_offset
+
+            # Translate special integer values for readability:
+            if key_value == 0:
+                key_value = "undefined"
+
+            elif key_value == 32767:
+                key_value = "user-defined"
+
+        if KeyValueTags(location) == KeyValueTags.GeoDoubleParamsTag:
+            tiff_tag = KeyValueTags.GeoDoubleParamsTag.value
+            offset = value_offset
+            key_value = image_tags[tiff_tag][offset]
+
+        if KeyValueTags(location) == KeyValueTags.GeoAsciiParamsTag:
+            tiff_tag = KeyValueTags.GeoAsciiParamsTag.value
+            offset = value_offset
+            ascii_val = image_tags[tiff_tag][offset:offset + count]
+            key_value = ascii_val.replace("|", "\n").strip()
+
+        geo_keys[key_name] = key_value
+
+    return geo_keys
 
 
 def img_slice(img, start_i, stop_i, start_j, stop_j):
