4fabba0de7ce853c4d154942e29f10d8e1f2dbb5
Magne Nordaas <magne.nordaas@gmail.com>
2019-01-04 15:45:29 +0100

    Drop unused argument from compute_shade

--- src/rasputin/calculate.py
+++ src/rasputin/calculate.py
@@ -6,10 +6,9 @@ from rasputin import triangulate_dem
 def compute_shade(*,
                   pts: triangulate_dem.PointVector,
                   faces: triangulate_dem.FaceVector,
-                  sun_ray: List[float],
-                  time: int) -> List[Tuple[int, np.ndarray]]:
+                  sun_ray: List[float]) -> np.ndarray:
     assert len(sun_ray) == 3
     shades = triangulate_dem.compute_shadow(pts, faces, sun_ray)
     has_shades = np.zeros(len(faces), dtype='int')
     has_shades[shades] = 1
-    return [(time, has_shades)]
+    return has_shades
