f4fc4f3b56b9bca32773c4ce2f2fbe813f825924
Magne Nordaas <magne.nordaas@gmail.com>
2019-01-08 09:56:02 +0100

    Minor fixes

--- src/rasputin/reader.py
+++ src/rasputin/reader.py
@@ -11,10 +11,12 @@ from pathlib import Path
 from logging import getLogger
 from rasputin import triangulate_dem
 
+
 class GeoTiffTags(Enum):
     ModelTiePointTag = 33922
     ModelPixelScaleTag = 33550
     GeoKeyDirectoryTag = 34735
+
 
 class KeyValueTags(Enum):
     # Parameter value types:
@@ -69,7 +71,7 @@ class GeoKeys(Enum):
     ProjCenterLongGeoKey = 3088
     ProjCenterLatGeoKey = 3089
     ProjCenterEastingGeoKey = 3090
-    ProjCenterNorthingGeoKey = 3091 # Incorrectly named in document
+    ProjCenterNorthingGeoKey = 3091  # Incorrectly named in document
     ProjScaleAtNatOriginGeoKey = 3092
     ProjScaleAtCenterGeoKey = 3093
     ProjAzimuthAngleGeoKey = 3094
@@ -87,16 +89,17 @@ def extract_geo_keys(*, image: TiffImageFile) -> Dict[str, Any]:
     image_tags = image.tag_v2
     geo_key_tag = GeoTiffTags.GeoKeyDirectoryTag.value
     try:
-        GeoKeyDirectory = np.asarray(image_tags[geo_key_tag], dtype="ushort").reshape((-1,4))
+        GeoKeyDirectory = np.asarray(image_tags[geo_key_tag],
+                                     dtype="ushort").reshape((-1, 4))
     except KeyError:
         raise RuntimeError("Image is missing GeoKeyDirectory required by GeoTiff v1.0.")
 
-    Header = GeoKeyDirectory[0,:]
+    Header = GeoKeyDirectory[0, :]
     KeyDirectoryVersion = Header[0]
     KeyRevision = (Header[1], Header[2])
     NumberOfKeys = Header[3]
 
-    assert KeyDirectoryVersion == 1 # Only existing version
+    assert KeyDirectoryVersion == 1  # Only existing version
     assert len(GeoKeyDirectory) == NumberOfKeys + 1
 
     # Read all the geokey fields
@@ -142,7 +145,6 @@ class GeoKeysParser(object):
         proj_str = GeoKeyParser(geokeys).to_proj4()
         proj = pyproj.Proj(proj_str)
 
-
     NOTE:
     This class is incomplete and many GeoKeys are not handled. Extending the class with new handlers
     or expanding existing ones should be mostly straight forward, but some care needs to be taken in
@@ -153,17 +155,16 @@ class GeoKeysParser(object):
         self.dict = dict()
         self.parse()
 
-
     def parse(self):
         # Try to extract a proj4 keyword argument for each (key, value) pair in the geokeys
-        unhandled = []
+        ignored_keys = []
         for (geokey_name, geokey_value) in self.geokeys.items():
             # Get handler
             try:
                 handler = getattr(self, f"_{geokey_name}")
             except AttributeError:
-                # Skip the GeoKey since no handler defined
-                unhandled.append(geokey_name)
+                # Skip the GeoKey when no handler defined
+                ignored_keys.append(geokey_name)
                 continue
 
             # Get a dict update from handler
@@ -181,9 +182,8 @@ class GeoKeysParser(object):
                 else:
                     self.dict[name] = val
 
-        # Report unhandled GeoKeys
         logger = getLogger()
-        logger.debug(f"Unhandled GeoKeys: {unhandled}")
+        logger.debug(f"Ignored GeoKeys: {ignored_keys}")
 
     def to_proj4(self) -> str:
         """
@@ -200,41 +200,35 @@ class GeoKeysParser(object):
 
         # Add no_defs to proj4 string to avoid use of default values.
         # Pyproj should raise an error for incomplete specification (e.g. missing ellps)
-        # Howerer, inconsistentencise may be ignored (e.g. when 'b' and 'rf' are both provided)
+        # Howerer, inconsistentencies may be ignored (e.g. when 'b' and 'rf' are both provided)
         proj4_str += " +no_defs"
 
         return proj4_str
-
 
     @staticmethod
     def _ProjectedCSTypeGeoKey(value):
         if _isinteger(value) and 20000 <= value <= 32760:
             return {"EPSG": value}
 
-
     @staticmethod
     def _GeoGraphicTypeGeo(value):
         if _isinteger(value) and 4000 <= value < 5000:
             return {"EPSG": value}
-
 
     @staticmethod
     def _GeogInvFlatteningGeoKey(value):
         if np.isscalar(value):
             return {"rf": float(value)}
 
-
     @staticmethod
     def _GeogPrimeMeridianLongGeoKey(value):
         if np.isscalar(value):
-            return {"pm" : float(value)}
-
+            return {"pm": float(value)}
 
     @staticmethod
     def _GeogSemiMajorAxisGeoKey(value):
         if np.isscalar(value):
-            return {"a" : float(value)}
-
+            return {"a": float(value)}
 
     @staticmethod
     def _ProjectionGeoKey(value):
@@ -251,21 +245,19 @@ class GeoKeysParser(object):
 
             return dict(proj="utm", zone=zone, south=south)
 
-
     @staticmethod
     def _ProjLinearUnitsGeoKey(value):
         # More units could be supported here
         unit_name = {9001: "m"}[value]
-        return {"units" : unit_name}
-
+        return {"units": unit_name}
 
     @staticmethod
     def _GeogCitationGeoKey(value):
         update = {}
 
         # Parse ellipsoid. GRS and WGS is probably enough
-        gcs_re = {"GRS":    ("GRS[ ,_]{0,1}(19|)\d\d", "\d\d$"),
-                  "WGS":    ("WGS[ ,_]{0,1}(19|)\d\d", "\d\d$"),
+        gcs_re = {"GRS":    ("GRS[ ,_]{0,1}(19|)\\d\\d", "\\d\\d$"),
+                  "WGS":    ("WGS[ ,_]{0,1}(19|)\\d\\d", "\\d\\d$"),
                   "sphere": ("sphere", None)}
 
         for (name, (pattern, postfix_pattern)) in gcs_re.items():
@@ -283,7 +275,7 @@ class GeoKeysParser(object):
         return update
 
 
-def identify_projection(image: TiffImageFile) -> str:
+def identify_projection(*, image: TiffImageFile) -> str:
     geokeys = extract_geo_keys(image=image)
     return GeoKeysParser(geokeys).to_proj4()
 
@@ -293,7 +285,7 @@ def img_slice(img, start_i, stop_i, start_j, stop_j):
     m, n = img.size
     if start_i > 0:
         try:
-            next(islice(myiter, start_i*n, start_i*n))
+            next(islice(myiter, start_i * n, start_i * n))
         except StopIteration:
             pass
     for i in range(stop_i - start_i):
@@ -311,7 +303,6 @@ def img_slice(img, start_i, stop_i, start_j, stop_j):
                 pass
 
 
-
 def read_raster_file(*,
                      filepath: Path,
                      x0: Optional[float] = None,
@@ -321,8 +312,10 @@ def read_raster_file(*,
     logger = getLogger()
     logger.debug(f"Reading raster file {filepath}")
     assert filepath.exists()
-    if x0 and x1: assert x0 < x1
-    if y0 and y1: assert y0 < y1
+    if x0 and x1:
+        assert x0 < x1
+    if y0 and y1:
+        assert y0 < y1
 
     with Image.open(filepath) as image:
         # Determine image extents
@@ -344,8 +337,8 @@ def read_raster_file(*,
 
         # Determine image coordinates, assuming here that the tie point is associated with pixel center.
         # Otherwise, coordinates should be shiftet half pixel size in both directions
-        X0, X1 = xt - jt * dx, xt + (n -1 - jt) * dx
-        Y1, Y0 = yt + it * dy, yt - (m -1 - it) * dy
+        X0, X1 = xt - jt * dx, xt + (n - 1 - jt) * dx
+        Y1, Y0 = yt + it * dy, yt - (m - 1 - it) * dy
 
         if x0 is None: x0 = X0
         if x1 is None: x1 = X1
@@ -354,16 +347,15 @@ def read_raster_file(*,
 
         # Identify pixel coordinates of view window extended to align with pixels
         j0 = max(int(np.floor((x0 - X0) / dx)), 0)
-        j1 = min(int(np.ceil((x1 - X0) / dx)), m-1)
+        j1 = min(int(np.ceil((x1 - X0) / dx)), m - 1)
         i0 = max(int(np.floor((Y1 - y1) / dy)), 0)
-        i1 = min(int(np.ceil((Y1 - y0) / dy)), n-1)
+        i1 = min(int(np.ceil((Y1 - y0) / dy)), n - 1)
 
         if j1 <= 0 or i1 <= 0 or i0 >= n or j0 >= m:
             # Empty view window, raise an error since PIL does not
             raise ValueError("Selected view window is outside of image bounds")
 
-        d = image.crop(box=(j0, i0, j1+1, i1+1))
-
+        d = image.crop(box=(j0, i0, j1 + 1, i1 + 1))
 
     # Get the actual coordinates of returned view window
     x0d, x1d = X0 + j0 * dx, X0 + j1 * dx
