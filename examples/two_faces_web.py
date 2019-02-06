f81e09017943d3ccfa3a10e0dffd1d6e9b4abafc
Vinzenz Gregor Eck <vinzenz@xal.no>
2019-02-06 16:47:19 +0100

    first simple version running

--- examples/two_faces_web.py
+++ examples/two_faces_web.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from rasputin import triangulate_dem
 from rasputin.html_writer import write_mesh
 
@@ -10,4 +12,6 @@ vs = triangulate_dem.PointVector([(1,0,0), (0,1,0), (0,0,0), (0.25,0.25,1)])
 points, faces = triangulate_dem.lindstrom_turk_by_ratio(vs, 2.0)
 normals = triangulate_dem.surface_normals(points, faces)
 
-write_mesh(pts=points, faces=faces, normals=normals)
+
+output_dir = Path.cwd() / "two_face_web"
+write_mesh(pts=points, faces=faces, normals=normals, output_dir=output_dir)
--- src/rasputin/html_writer.py
+++ src/rasputin/html_writer.py
@@ -1,6 +1,9 @@
 from pathlib import Path
+import shutil
 
 from rasputin import triangulate_dem
+
+
 
 
 def write_mesh(*,
@@ -11,5 +14,29 @@ def write_mesh(*,
     """
     This function writes a output html web-site code for the give mesh to output dir.
     """
-    pass
+    if output_dir.exists():
+        shutil.rmtree(str(output_dir))
 
+    templates_path = Path(__file__).parent.parent.parent / "web"
+
+    # copy all template files
+    shutil.copytree(str(templates_path), str(output_dir))
+
+    data_js = output_dir / "data.js"
+    with data_js.open(mode="w") as f:
+        f.write("const indices = [\n")
+        for face in faces:
+            f.write(f"{', '.join([str(f) for f in face])},\n")
+        f.write("\n];\n\n")
+        f.write("\nconst vertices = new Float32Array( [\n")
+        for p in pts:
+            f.write(f"{', '.join([str(s) for s in p])},\n")
+        f.write("\n] );\n\n")
+        f.write("const colors = new Float32Array( [\n")
+        for face in faces:
+            f.write("0.0,  1.0,  0.0,\n")
+            f.write("1.0,  0.0,  0.0,\n")
+            f.write("0.0,  0.0,  1.0,\n\n")
+        f.write("\n] );\n\n")
+
+        f.write("\nconst data = {indices, vertices, colors};")
