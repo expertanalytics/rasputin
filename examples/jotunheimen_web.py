355915c7d28a0d29171c447b0f35299fa86a284c
Ola Skavhaug <ola@xal.no>
2019-03-26 08:48:34 +0100

    Added some todos and docs.

--- examples/jotunheimen_web.py
+++ examples/jotunheimen_web.py
@@ -3,31 +3,41 @@ import numpy as np
 import PIL
 
 from rasputin import triangulate_dem
-from rasputin.html_writer import (write_mesh, color_field_by_height, color_field_by_slope,
-                                  color_field_by_aspect, add_slope_colors, color_field_by_avalanche_danger,
-                                  mesh_with_avalanche_danger)
+from rasputin.html_writer import (write_mesh, add_slope_colors, color_field_by_avalanche_danger)
 from rasputin.reader import read_raster_file, extract_geo_keys, GeoKeysInterpreter
 from rasputin.triangulate_dem import lindstrom_turk_by_ratio
 from rasputin import avalanche
 
 
 """
-Most minimal example
+Avalance Forecast Visualization Example.
+
+Items to develop:
+ * Accept an area of interest as start_x, start_y, stop_x, stop_y, with coordinate system
+ * Construct several color maps for each avalanche danger, and make the web app offer selections
+ * Alternative approach is to partition the whole area into disjoint toplogies and switch between these
+ * Use better texturing for different terrain types (under development)
+ * Use more of the information from varsom (and perhaps alpha blending) to better display avalanche dangers
+ 
 """
 
+# Define area of interest
+# Orig data set size
+x0 = 432000.0
+x1 = 491990.0
+y0 = 6790010.0
+y1 = 6850000.0
+
+# Select a smaller part of the raster
+y1 = y0 + (y1 - y0)/10
+x1 = x0 + (x1 - x0)/10
+
+# TODO: Use the lat-lon from area of interest to read raster files from senorge files.
 raster_file = Path("../../rasputin_data/jotunheimen.tif")
 assert raster_file.exists()
 
 interp = GeoKeysInterpreter(extract_geo_keys(image=PIL.Image.open(raster_file)))
 
-# Orig data set size
-x0 = 432000.0
-x1 = 491990.0 
-y0 = 6790010.0 
-y1 = 6850000.0
-
-y1 = y0 + (y1 - y0)/10
-x1 = x0 + (x1 - x0)/10
 dangers = []
 expositions = "00000000"
 
@@ -70,16 +80,12 @@ points, faces = lindstrom_turk_by_ratio(raster_coords, 0.5)
 
 normals = triangulate_dem.surface_normals(points, faces)
 point_normals = triangulate_dem.point_normals(points, faces)
-face_field = color_field_by_slope(normals=normals)
 
-face_field2 = color_field_by_aspect(normals=normals)
-face_field2 = add_slope_colors(normals=normals, colors=face_field2)
-
-meshes = mesh_with_avalanche_danger(points=points, faces=faces, normals=normals,
-                                    avalanche_problems=avalanche_problems)
+# Commenting these out for now, as we need to partition the mesh into disjoint topologies for it to work.
+#meshes = mesh_with_avalanche_danger(points=points, faces=faces, normals=normals,
+#                                    avalanche_problems=avalanche_problems)
 
 colors = np.ones(np.asarray(faces).shape)
-
 colors = add_slope_colors(normals=normals, colors=colors)
 colors = color_field_by_avalanche_danger(normals=normals,
                                               points=points,
